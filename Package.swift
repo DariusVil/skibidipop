51c1974de157915a8ed2409e745ea1319a126fc4
Darius Vilcinskis <darius.vilcinskis@gmail.com>
2023-11-11 12:47:11 +0200

    Add argument handling

--- Package.swift
+++ Package.swift
@@ -4,9 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "skibidipop",
+    dependencies: [
+      .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+    ],
     targets: [
         .executableTarget(
             name: "skibidipop",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
             path: "Sources"
         ),
         .testTarget(
--- Sources/skibidipop.swift
+++ Sources/skibidipop.swift
@@ -1,8 +1,35 @@
-@main
-struct FigletTool {
+import ArgumentParser
 
-    static func main() {
+@main
+struct FigletTool: ParsableCommand {
+
+    @Argument(help: "Command")
+    var command: String
+
+    @Argument(help: "Specify Branch name")
+    var branchName: String?
+
+    func run() throws {
         let controller = CompositionRoot.controller
+
+        switch command {
+            case "sync":
+                controller.sync()
+            case "list":
+                controller.list()
+            case "rebase":
+                controller.rebase()
+            case "chain":
+                guard let branchName else {
+                    print("Please specify branch name")
+
+                    return
+                }
+
+                controller.chain(branch: branchName)
+            default:
+                print("Unknown command")
+        }
     }
 }
 
