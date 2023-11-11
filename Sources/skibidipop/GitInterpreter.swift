f5a7c1cc8a0fa35d86a9befa37003561e5234dc4
Darius Vilcinskis <darius.vilcinskis@gmail.com>
2023-11-11 17:40:29 +0200

    Remove unused code

--- Sources/skibidipop/GitInterpreter.swift
+++ Sources/skibidipop/GitInterpreter.swift
@@ -1,28 +1,10 @@
 protocol GitInterpreting {
 
-    func chain(branch: String)
-    func sync()
-    func list()
-    func rebase()
+    
 }
 
 struct GitInterpreter {}
 
 extension GitInterpreter: GitInterpreting {
 
-    func chain(branch: String) {
-        fatalError("Not implemented")
-    }
-
-    func sync() {
-        fatalError("Not implemented")
-    }
-
-    func list() {
-        fatalError("Not implemented")
-    }
-
-    func rebase() {
-        fatalError("Not implemented")
-    }
 }
