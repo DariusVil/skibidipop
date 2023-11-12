c76ea0e8d94c01d092ab61a49bfe10149936ee7d
Darius Vilcinskis <darius.vilcinskis@gmail.com>
2023-11-12 12:43:28 +0200

    Add rebase and initialize git commands

--- Sources/skibidipop/GitInterpreter.swift
+++ Sources/skibidipop/GitInterpreter.swift
@@ -1,10 +1,42 @@
+import Foundation
+
 protocol GitInterpreting {
 
-    
+    func initialize()
+    func rebase(onto branch: String)
 }
 
 struct GitInterpreter {}
 
 extension GitInterpreter: GitInterpreting {
 
+    func initialize() {
+        execute(["init"])
+    }
+
+    func rebase(onto branch: String) {
+        execute(["rebase", "master"])
+    }
+
+    private func execute(_ arguments: [String]) {
+        let task = Process()
+        task.executableURL = URL(string: "/usr/bin/git")
+        task.arguments = arguments
+
+        let pipe = Pipe()
+        task.standardOutput = pipe
+
+        do {
+            try task.run()
+        } catch {
+
+        }
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        if let output = String(data: data, encoding: .utf8) {
+            print(output)
+        }
+
+        task.waitUntilExit()    
+    }
 }
